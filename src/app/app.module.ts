098e45ccb9124c7a8195a90d88626fdf34718215
Ahmed Ibrahim <ebrahema89859@gmail.com>
2023-07-12 18:38:45 +0300

    Finial Project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CommonModule } from '@angular/common';
 import { ProductsModule } from './products/products.module';
 import { CartsModule } from './carts/carts.module';
 import { AdminModule } from './admin/admin.module';
+import { AppComponent } from './App/app.component';
 
 @NgModule({
   declarations: [
